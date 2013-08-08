51f6890936afd4e60b616ac09c30ce391f603d73
Dan Brooks <danger.dan_@hotmail.co.uk>
2013-08-09 00:21:20 +0100

    Improved Run Markdown algo

--- MarkdownToOpenXML.cs
+++ MarkdownToOpenXML.cs
@@ -25,6 +25,18 @@ namespace MarkdownToOpenXML
             set { CustomMode = value; }
         }
 
+        private enum rExKey
+        {
+            DblAsterisk, Asterisk, Grave, Underscore
+        }
+
+        private static Dictionary<rExKey, Regex> Patterns = new Dictionary<rExKey, Regex>()
+        {
+            { rExKey.DblAsterisk, new Regex("(?<!\\*)(\\*\\*)([^\\ +].+?)(\\*\\*)") },
+            { rExKey.Asterisk, new Regex("(?<!\\*)[^\\*]?(\\*)([^\\*].+?)(\\*)[^\\*]") },
+            { rExKey.Grave, new Regex("(?<!`)[^`]?(`)([^`].+?)(\\`)[^`]?") },
+            { rExKey.Underscore, new Regex("(?<!_)[^_]?(_)([^_].+?)(_)[^_]?") },
+        };
 
         public static void CreateDocX(string content, string docName)
         {
@@ -32,125 +44,7 @@ namespace MarkdownToOpenXML
             Body body = Convert(md);
             SaveDocX(body, docName);
         }
-
-        private static ParagraphProperties GenerateParagraphProperties(string lookahead)
-        {
-            ParagraphProperties pPr = new ParagraphProperties();
-            Match isHeader1 = Regex.Match(Buffer, @"^#");
-            String sTest = Regex.Replace(lookahead, @"\w", "");
-            Match isSetextHeader1 = Regex.Match(sTest, @"[=]{2,}");
-
-            // Set Paragraph Styles
-            if (isHeader1.Success || isSetextHeader1.Success)
-            {
-                ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "Heading1" };
-                pPr.Append(paragraphStyleId1);
-
-                if (isHeader1.Success) Buffer = Buffer.Substring(1);
-
-                if (isSetextHeader1.Success) SkipNextLine = true;
-            }
-
-            return pPr;
-        }
-
-        private static Body Convert(List<string> md)
-        {
-            Body body = new Body();
-            SkipNextLine = false;
-            Buffer = @"";
-            int index = 0;
-            foreach (string line in md)
-            {
-                if (SkipNextLine)
-                {
-                    index += 1;
-                    SkipNextLine = !SkipNextLine;
-                    continue;
-                }
-
-                Buffer = line;
-                string lookahead = index + 1 != md.Count ? md[index + 1] : "";
-
-                ParagraphProperties pPr = GenerateParagraphProperties(lookahead);
-                Paragraph p = new Paragraph();
-                p.Append(pPr);
-
-                // Set Run Styles :-
-                // Here it needs to weigh up what combo of formatting
-                // each run is going to have before 'Committing' the
-                // run to the paragraph
-                bool bOn = false;
-                bool iOn = false;
-                bool uOn = false;
-
-                int CurrentPosition = 0;
-                int cropSymbol = 0;
-
-                Regex pattern = CustomMode
-                    ? new Regex(@"(\*\*|`|_)")
-                    : new Regex(@"(\*\*|\*)");
-
-                Match m = pattern.Match(Buffer);
-                Run run = new Run();
-
-                if (m.Success)
-                {
-                    RunProperties rPr = new RunProperties();
-                    run.Append(new Text(Buffer.Substring(CurrentPosition, m.Index)));
-
-                    while (m.Success)
-                    {
-                        if (!CustomMode)
-                        {
-                            if (m.ToString() == "**") bOn = !bOn;
-                            if (m.ToString() == "*") iOn = !iOn;
-                        }
-                        else
-                        {
-                            if (m.ToString() == "**") bOn = !bOn;
-                            if (m.ToString() == "`") iOn = !iOn;
-                            if (m.ToString() == "_") uOn = !uOn;
-                        }
-
-                        rPr = new RunProperties();
-
-                        rPr.Append(new Bold() { Val = new OnOffValue(bOn) });
-                        if (iOn) rPr.Append(new Italic());
-                        if (uOn) rPr.Append(new Underline() { Val = DocumentFormat.OpenXml.Wordprocessing.UnderlineValues.Single });
-                        run.Append(rPr);
-
-                        CurrentPosition = m.Index;
-                        cropSymbol = CurrentPosition + m.Length;
-                        m = m.NextMatch();
-
-                        if (m.Index == 0)
-                        {
-                            run.Append(new Text(Buffer.Substring(cropSymbol)));
-                        }
-                        else
-                        {
-                            run.Append(new Text(Buffer.Substring(cropSymbol, m.Index - cropSymbol)));
-                        }
-
-                        p.Append(run);
-                        run = new Run();
-                    }
-                }
-                else
-                {
-                    run.Append(new Text(Buffer));
-                    p.Append(run);
-                }
-
-                body.Append(p);
-
-                index += 1;
-            }
-
-            return body;
-        }
-
+        
         private static List<string> StripDoubleLines(string text)
         {
             List<string> md = new List<string>();
@@ -181,8 +75,159 @@ namespace MarkdownToOpenXML
 
                 md.Add(tmp);
             }
-            
+
             return md;
+        }
+
+        private static Body Convert(List<string> md)
+        {
+            Body body = new Body();
+            SkipNextLine = false;
+            Buffer = @"";
+            int index = 0;
+            
+            foreach (string line in md)
+            {
+                if (SkipNextLine)
+                {
+                    index += 1;
+                    SkipNextLine = !SkipNextLine;
+                    continue;
+                }
+
+                Buffer = line;
+                string lookahead = index + 1 != md.Count ? md[index + 1] : "";
+
+                ParagraphProperties pPr = GenerateParagraphProperties(lookahead);
+                Paragraph p = new Paragraph();
+                p.Append(pPr);
+                
+                p = GenerateRuns(p, Buffer);
+                body.Append(p);
+                index += 1;
+            }
+
+            return body;
+        }
+
+        private static Ranges<int> FindMatches(Regex rx, string s, ref Ranges<int> TokenRange)
+        {
+            Ranges<int> ranges = new Ranges<int>();
+            MatchCollection mc = rx.Matches(s);
+            
+            foreach (Match m in mc)
+            {
+                int sToken = m.Groups[1].Index;
+                int match = m.Groups[2].Index;
+                int eToken = m.Groups[3].Index;
+                int endStr = m.Groups[3].Index + m.Groups[3].Length;
+
+                Range<int> StartToken = new Range<int>();
+                StartToken.Minimum = sToken;
+                StartToken.Maximum = match - 1;
+                TokenRange.add(StartToken);
+
+                Range<int> MatchRange = new Range<int>();
+                MatchRange.Minimum = match;
+                MatchRange.Maximum = eToken - 1;
+                ranges.add(MatchRange);
+
+                Range<int> EndToken = new Range<int>();
+                EndToken.Minimum = eToken;
+                EndToken.Maximum = endStr - 1;
+                TokenRange.add(EndToken);
+            }
+
+            return ranges;
+        }
+
+        private static Paragraph GenerateRuns(Paragraph p, string Buffer)
+        {
+            // Calculate positions of all tokens and use this to set 
+            // run styles when iterating through the string
+
+            // in the same calculation note down location of tokens 
+            // so they can be ignored when loading strings into the buffer
+            
+            Regex pBold, pItalic, pUnderline;
+
+            if (!CustomMode)
+            {
+                pBold = Patterns[rExKey.DblAsterisk];
+                pItalic = Patterns[rExKey.Asterisk];
+                pUnderline = Patterns[rExKey.Underscore];
+            }
+            else
+            {
+                pBold = Patterns[rExKey.DblAsterisk];
+                pItalic = Patterns[rExKey.Grave];
+                pUnderline = Patterns[rExKey.Underscore];
+            }
+
+            Ranges<int> Tokens = new Ranges<int>();
+            Ranges<int> Bold = FindMatches(pBold, Buffer, ref Tokens);
+            Ranges<int> Italic = FindMatches(pItalic, Buffer, ref Tokens);
+            Ranges<int> Underline = FindMatches(pUnderline, Buffer, ref Tokens);
+
+            if ((Bold.Count() + Italic.Count() + Underline.Count()) == 0)
+            {
+                Run run = new Run();
+                run.Append(new Text(Buffer)
+                {
+                    Space = SpaceProcessingModeValues.Preserve
+                });
+                p.Append(run);
+            }
+            else
+            {
+                int CurrentPosition = 0;
+                Run run;
+
+                while (CurrentPosition < Buffer.Length)
+                {
+                    if (!Tokens.ContainsValue(CurrentPosition))
+                    {
+                        run = new Run();
+
+                        RunProperties rPr = new RunProperties();
+                        if (Bold.ContainsValue(CurrentPosition)) rPr.Append(new Bold() { Val = new OnOffValue(true) });
+                        if (Italic.ContainsValue(CurrentPosition)) rPr.Append(new Italic());
+                        if (Underline.ContainsValue(CurrentPosition)) rPr.Append(new Underline() { Val = DocumentFormat.OpenXml.Wordprocessing.UnderlineValues.Single });
+                        run.Append(rPr);
+
+                        string TextBuffer = Buffer.Substring(CurrentPosition, 1);
+                        run.Append(new Text(TextBuffer)
+                        {
+                            Space = SpaceProcessingModeValues.Preserve
+                        });
+                        p.Append(run);
+                    }
+
+                    CurrentPosition++;
+                }
+            }
+            return p;
+        }
+
+        private static ParagraphProperties GenerateParagraphProperties(string lookahead)
+        {
+            ParagraphProperties pPr = new ParagraphProperties();
+            Match isHeader1 = Regex.Match(Buffer, @"^#");
+            String sTest = Regex.Replace(lookahead, @"\w", "");
+            Match isSetextHeader1 = Regex.Match(sTest, @"[=]{2,}");
+
+            // Set Paragraph Styles
+            if (isHeader1.Success || isSetextHeader1.Success)
+            {
+                ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "Heading1" };
+                pPr.Append(paragraphStyleId1);
+
+                if (isHeader1.Success) Buffer = Buffer.Substring(1);
+
+                if (isSetextHeader1.Success) SkipNextLine = true;
+            }
+
+            return pPr;
         }
 
         private static void SaveDocX(Body body, String docName)
@@ -199,6 +244,46 @@ namespace MarkdownToOpenXML
 
                 package.MainDocumentPart.Document.AppendChild(body);
                 package.MainDocumentPart.Document.Save();
+            }
+        }
+        
+        public class Ranges<T> where T : IComparable<T>
+        {
+            private List<Range<T>> rangelist = new List<Range<T>>();
+
+            public void add(Range<T> range)
+            {
+                rangelist.Add(range);
+            }
+
+            public int Count()
+            {
+                return rangelist.Count;
+            }
+
+            public Boolean ContainsValue(T value)
+            {
+                foreach (Range<T> range in rangelist)
+                {
+                    if (range.ContainsValue(value)) return true;
+                }
+
+                return false;
+            }
+        }
+        
+        public class Range<T> where T : IComparable<T>
+        {
+            public T Minimum { get; set; }
+            public T Maximum { get; set; }
+            
+            public override string ToString() { return String.Format("[{0} - {1}]", Minimum, Maximum); }
+
+            public Boolean IsValid() { return Minimum.CompareTo(Maximum) <= 0; }
+
+            public Boolean ContainsValue(T value)
+            {
+                return (Minimum.CompareTo(value) <= 0) && (value.CompareTo(Maximum) <= 0);
             }
         }
     }
